7ebb2a7857db1c1050e4a678c4df89b753452a7f
otaku <2512975065@qq.com>
2023-04-04 16:28:01 +0800

    Update CommonMethod Overloading

--- 2.对象、权限、包、构造方法和方法重载/src/Class1.java
+++ 2.对象、权限、包、构造方法和方法重载/src/Class1.java
@@ -8,6 +8,12 @@ public class Class1 {
 	public Class1(int a) {
 		num=a;	//Make variable "num" same as varibale "a"
 	}
+	public static void commomMethodOverloadingExample() {
+		System.out.println("Overloading Method:commomMethodOverloadingExample() Called!");
+	}
+	public static void commomMethodOverloadingExample(int a) {
+		System.out.println("Overloading Method:commomMethodOverloadingExample(int a) Called! The variable \"a\" is:"+a);
+	}
 	public  static void  printInfo() {
 		System.out.println("This is default package.");
 		Class2 dfPkg = new Class2();
@@ -15,7 +21,9 @@ public class Class1 {
 		Class1 samePkg = new Class1();// Moot method
 		System.out.println("After new an constructed instance with no parameters ,the value of variable \"num\" is " + num);
 		Class1 samePkg2 = new Class1(10);
-		System.out.println("The value of variable \"num\" is " + num);
+		System.out.println("Constructor Overloading. The value of variable \"num\" is " + num);
+		commomMethodOverloadingExample();
+		commomMethodOverloadingExample(114514);
 	}
 	public static void main(String[] args) {
 		printInfo();
