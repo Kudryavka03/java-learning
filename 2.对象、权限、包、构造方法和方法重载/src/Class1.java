0e4971fe47b88d6d507d07e4ab749d89455617d5
otaku <2512975065@qq.com>
2023-04-04 16:17:23 +0800

    Update 对象、权限、包、构造方法和方法重载

---
+++ 2.对象、权限、包、构造方法和方法重载/src/Class1.java
@@ -0,0 +1,23 @@
+import Package2.Class2; //Class2 in Package2
+
+public class Class1 {
+	static int num;	//define a variable for Class1
+	public Class1() {
+		
+	}
+	public Class1(int a) {
+		num=a;	//Make variable "num" same as varibale "a"
+	}
+	public  static void  printInfo() {
+		System.out.println("This is default package.");
+		Class2 dfPkg = new Class2();
+		dfPkg.printInfo();
+		Class1 samePkg = new Class1();// Moot method
+		System.out.println("After new an constructed instance with no parameters ,the value of variable \"num\" is " + num);
+		Class1 samePkg2 = new Class1(10);
+		System.out.println("The value of variable \"num\" is " + num);
+	}
+	public static void main(String[] args) {
+		printInfo();
+	}
+}
---
+++ 2.对象、权限、包、构造方法和方法重载/src/Package2/Class2.java
@@ -0,0 +1,7 @@
+package Package2;
+
+public class Class2 {
+	public static void  printInfo() {
+		System.out.println("This is Package2 in-built method called from Class1");
+	}
+}
