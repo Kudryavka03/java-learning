b7404df55d45e1482d31621f132b6e4c743d96cc
otaku <2512975065@qq.com>
2023-05-24 04:10:20 +0800

    0

--- Other/StudentExample1/src/main/java/org/example/Main.java
+++ Other/StudentExample1/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -40,10 +40,63 @@ public class Main {
             }
             classAvgScore.add(avgScore / (double)people );          //计算平均分并加入classAvgScore里
         }
-
         IntStream.range(0,className.size()).forEach
                 (i->System.out.println("班级："+className.get(i).toString()+" 平均成绩:"+classAvgScore.get(i).toString()));
             //打印相关信息。由于上述算法是严格遵循顺序来的，所以直接按照顺序打印出两个变量即可，亦可以用TreeList排序
             //也可以用TreeMap，新建一个方法专门用来计算平均成绩也可以
+        System.out.println("\n");
+        main2();
     }
+    public static void main2(){
+        List<Student> list = new ArrayList<Student>() {{
+            add(new Student("Tom", 18, 100, "class05"));
+            add(new Student("Jerry", 22, 70, "class04"));
+            add(new Student("Owen", 23, 90, "class05"));
+            add(new Student("Jim", 25, 80, "class05"));
+            add(new Student("Steve", 20, 66, "class06"));
+            add(new Student("Kevin", 24, 100, "class04"));
+        }};
+        /*
+        Comparator<Student> sortByClassNum = new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getClassName().compareTo(o2.getClassName());
+            }
+        };
+        Collections.sort(list, sortByClassNum);
+        */
+         
+
+        // 获取平均年龄
+        double averageAge = getAverageAge(list);
+        System.out.println("所有学生平均年龄：" + averageAge);
+
+        // 获取班级平均成绩
+        Map<String, Double> classAvgScoreMap = getClassAvgScoreMap(list);
+        for (Map.Entry<String, Double> entry : classAvgScoreMap.entrySet()) {
+            System.out.println("班级：" + entry.getKey() + " 平均成绩：" + entry.getValue());
+        }
+    }
+    private static double getAverageAge(List<Student> list) {
+        return list.stream()
+                .mapToInt(Student::getAge)
+                .average()
+                .orElse(0);
+    }
+
+    private static Map<String, Double> getClassAvgScoreMap(List<Student> list) {
+        Map<String, List<Double>> classScoreMap = list.stream().sorted()             //后面主要是对成绩进行收集
+                .collect(Collectors.groupingBy(Student::getClassName,       //分组，基于ClassName和
+                        Collectors.mapping(Student::getScore, Collectors.toList())));   //（对成绩进行收集并弄成一个新的表）
+  //                                                  拿成绩         到新表
+        //这个Map就是这样的：("class05",{100,90,80})
+        return classScoreMap.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,//遍历拿classScoreMap的Key，就是班级名称
+                        x -> x.getValue().stream()//Stream API，此时x是entry
+                                .mapToDouble(i -> i)//遍历到Double（变量i）
+                                .average()          //求i平均
+                                .orElse(0)
+                ));   //默认值：0
+    }
+
 }
--- Other/StudentExample1/src/main/java/org/example/Student.java
+++ Other/StudentExample1/src/main/java/org/example/Student.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Student {
+public class Student implements Comparable<Student> {
     String name;
     int age;
     double score;
@@ -12,5 +12,36 @@ public class Student {
         this.score = score;
         this.classNum = classNum;
     }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public double getScore(){
+        return score;
 
+    }
+    public String getClassName(){
+        return classNum;
+    }
+
+    @Override
+    public int compareTo(Student o1){
+        return this.getClassName().compareTo(o1.getClassName());
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==null){
+            return false;
+        }
+        if(!(obj instanceof Student)){
+            return false;
+        }
+        if(this == obj){
+            return true;
+        }
+        Student student = (Student) obj;
+        return (this.classNum.equals(student.getClassName()));
+    }
 }
